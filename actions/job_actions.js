e7fdb94118ff7a541c313afdf2ef5c6ff9eb6c60
Graeme <graemster@gmail.com>
2020-01-06 13:16:44 -0600

    add job action

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -2,15 +2,16 @@ import JOB_DATA from "./IndeedJobData.json";
 import { FETCH_JOBS } from "./types";
 import axios from "axios";
 
-const JOBS_ROOT_URL = "https://jobs.github.com/positions.json?"
+const JOBS_ROOT_URL = "https://jobs.github.com/positions.json?";
 
 export const fetchJobs = (region, callback) => async dispatch => {
   try {
     //let zip = await reverseGeoCode(region);
     //const url = buildJobsUrl(zip);
-    //let { data } = await axios.get(url);
-    const data = JOB_DATA;
-    //console.log(data);
+    const url = `${JOBS_ROOT_URL}lat=${region.latitude}&&long=${region.longitude}`;
+    let { data } = await axios.get(url);
+    // const data = JOB_DATA;
+    console.log(data);
     dispatch({ type: FETCH_JOBS, payload: data });
     callback();
   } catch (e) {
